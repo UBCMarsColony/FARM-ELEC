e1d39a61160a754a6cf6e82782b539803fb95322
Felix the Cat <58372205+CFelixZhao@users.noreply.github.com>
2022-01-29 14:14:52 -0800

    combined code has 2 bme, 2 co2 sensors

--- combinedCode/main.ino/main/main.ino
+++ combinedCode/main.ino/main/main.ino
@@ -41,9 +41,6 @@
 //#include "kSeries.h"
 #include <SoftwareSerial.h>
 
-// DHT22
-#include <DHT.h>
-
 // BME280
 #include <SPI.h>
 #include <Adafruit_Sensor.h>
@@ -57,29 +54,27 @@
 #define CO2_LOWER_THRESHOLD 1100        // Threshold below which to enable pump
 #define CO2_UPPER_THRESHOLD 1300        // Threshold above which to enable pump
 #define PUMP 1                          // Pump pin connection
-#define DHTPIN 7                        // What pin we're connected to
-#define DHTTYPE DHT22                   // DHT 22  (AM2302)
-#define SEALEVELPRESSURE_HPA (1013.25)  // Constant sea level pressure
 
 // Initializations
 SoftwareSerial portOne(Rx1, Tx1);    // Initialize CO2 sensor for kSeries k30 (control)
 SoftwareSerial portTwo(Rx2, Tx2);    // Initialize CO2 sensor for kSeries k30 (lpgc)
 
-DHT dht(DHTPIN, DHTTYPE); // Initialize DHT sensor for normal 16mhz Arduino
-Adafruit_BME280 bme;      // Initialize BME280 for I2C (Adafruit code)
+Adafruit_BME280 bme_control;      // Initialize BME280 for I2C (control)
+Adafruit_BME280 bme_lpgc;      // Initialize BME280 for I2C (lpgc)
 
 // Variables
-float co2_control; //Stores CO2 value (control)
-float co2_lpgc;    //Stores CO2 value (lpgc)
-float hum;  //Stores humidity value
-float temp; //Stores temperature value
-float pres; //Stores pressure value
-float hum_dht;  //Stores humidity value from DHT
-float temp_dht; //Stores temperature value from DHT
+float co2_control;  //Stores CO2 value (control)
+float co2_lpgc;     //Stores CO2 value (lpgc)
+float hum_control;  //Stores humidity value (control)
+float hum_lpgc;     //Stores humidity value (lpgc)
+float temp_control; //Stores temperature value (control)
+float temp_lpgc;    //Stores temperature value (lpgc)
+float pres_control; //Stores pressure value (control)
+float pres_lpgc;    //Stores pressure value (lpgc)
 
-unsigned long delayTime = 5000;  //Current delay time between readings
-int bmeAddr = 0x76;               //BME280 sensor address
-int bmeAddr2 = 0x77;              //BME280 sensor address for second BME sensor
+unsigned long delayTime = 5000;   //Current delay time between readings
+int bmeAddr_control = 0x76;       //BME280 sensor address for control chamber
+int bmeAddr_lpgc = 0x77;          //BME280 sensor address for lpgc
 byte readCO2[] = {0xFE, 0X44, 0X00, 0X08, 0X02, 0X9F, 0X25}; //Command packet to read Co2 (see app note) 
 byte response[] = {0,0,0,0,0,0,0}; //create an array to store the response 
 
@@ -101,8 +96,9 @@ void setup() {
   // Start each software serial port
   portOne.begin(9600);
   portTwo.begin(9600);
-  dht.begin(); // DHT backup
-  bme.begin(bmeAddr, &Wire);
+  bme_control.begin(0x76, &Wire);
+  bme_lpgc.begin(0x77, &Wire);
+
 }
 
 // START OF MAIN LOOP --------------------------------------------------------- 
@@ -120,19 +116,23 @@ void loop()
 void processSensors() 
 {
   // Read sensor inputs
-  hum_dht = dht.readHumidity();                 // DHT backup
-  temp_dht = dht.readTemperature();             // DHT backup
-//  co2_control = k30_control.getCO2('p');        // Read CO2 (K30 control)
-//  co2_lpgc = k30_lpgc.getCO2('p');              // Read CO2 (K30 lpgc)
-  hum = bme.readHumidity();                     // Read humidity (BME)
-  temp = bme.readTemperature();                 // Read temperature (BME)
-  pres = bme.readPressure() / 100.0F;           // Read pressure (BME)
 
+  // Control BME
+  hum_control = bme_control.readHumidity();                     // Read humidity
+  temp_control = bme_control.readTemperature();                 // Read temperature
+  pres_control = bme_control.readPressure() / 100.0F;           // Read pressure
+
+  // LPGC BME
+  hum_lpgc = bme_lpgc.readHumidity();                        // Read humidity
+  temp_lpgc = bme_lpgc.readTemperature();                    // Read temperature
+  pres_lpgc = bme_lpgc.readPressure() / 100.0F;              // Read pressure
+
+  // Listen to first CO2 port
   portOne.listen();
   sendRequestA(readCO2); 
   co2_control = getValueA(response); 
 
-  // Now listen on the second port
+  // Listen to second CO2 port
   portTwo.listen();
   sendRequestB(readCO2); 
   co2_lpgc = getValueB(response);   
@@ -152,22 +152,25 @@ void sendDataToSerial()
   Serial.print(co2_control);
   Serial.print(kDelimiter);
   
+  Serial.print(hum_control);
+  Serial.print(kDelimiter);
+
+  Serial.print(temp_control);
+  Serial.print(kDelimiter);
+
+  Serial.print(pres_control);
+  Serial.print(kDelimiter);
+
   Serial.print(co2_lpgc);
   Serial.print(kDelimiter);
-  
-  Serial.print(hum);
+
+  Serial.print(hum_lpgc);
   Serial.print(kDelimiter);
 
-  Serial.print(temp);
+  Serial.print(temp_lpgc);
   Serial.print(kDelimiter);
 
-  Serial.print(pres);
-  Serial.print(kDelimiter);
-
-  Serial.print(hum_dht);
-  Serial.print(kDelimiter);
-
-  Serial.print(temp_dht);
+  Serial.print(pres_lpgc);
   Serial.print(kDelimiter);
   
   Serial.println(); // Add final line ending character only once
