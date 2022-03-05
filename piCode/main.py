feacf094c7535cc7627277b26965407dc10a627e
AWadhw <azaanwadhwania@yahoo.co.in>
2022-03-05 13:04:44 -0800

    data logging works

--- piCode/main.py
+++ piCode/main.py
@@ -1,20 +1,19 @@
 import serial
 import csv
-PATH = "/media/usb/file.csv"
+PATH = "/home/pi/ubcfarm/FARM-electrical/data/data.csv"
 
 
 def writeToCSV(string):
-    f = open(PATH)
-    writer = csv.writer(f)
-    writer.writerow(string)
-    f.close()
-    
+    	f = open(PATH, 'a')
+    	writer = csv.writer(f)
+    	writer.writerow(string)
+    	f.close()
+
 if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-    ser.reset_input_buffer()
-    while True:
-        if ser.in_waiting > 0:
-            line = ser.readline().decode('utf-8').rstrip()
-            writeToCSV(line)
-
-
+    	ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+    	ser.reset_input_buffer()
+	while True:
+		if ser.in_waiting > 0:
+			line = ser.readline().decode('utf-8').rstrip().split(',')
+			#control: co2, hum, temp, pres, lpgc: co2, hum, temp, pres
+			writeToCSV(line) 
